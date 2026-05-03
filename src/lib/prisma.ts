667d19b687e811478dc669ea85923fddcdeea32a
BasByte <netdroid007@gmail.com>
2026-05-04 00:46:44 +0100

    Prisma url commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
+import "dotenv"
 
 declare global {
   var _prisma: PrismaClient | undefined;
